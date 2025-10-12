11d395d1cc2c955aa28bfb46d37bb9e33df5c5f6
Lagnajit Ghosh <lagnag123@gmail.com>
2025-10-12 11:13:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4013845559-fdc57",
+  "appId": "1:715012482827:web:669fadd7f2a86ee351240e",
+  "apiKey": "AIzaSyD8eV3dNVteZb_S3Nb3NVb0n5NZmA7OEto",
+  "authDomain": "studio-4013845559-fdc57.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "715012482827"
+};
