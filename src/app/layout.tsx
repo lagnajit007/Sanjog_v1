485df634bb08acdb3c0f5d9d0b49e0e3f6b34657
Lagnajit Ghosh <lagnag123@gmail.com>
2025-10-12 21:50:03 +0000

    also why the landing page is getting dark in open in new window view fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light scroll-smooth" suppressHydrationWarning>
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
