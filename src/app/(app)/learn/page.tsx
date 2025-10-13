c3c50977b11e41e568e5060a18eaf1a25b1de774
Lagnajit Ghosh <lagnag123@gmail.com>
2025-10-13 20:28:38 +0000

    Try fixing this error: `Console Error: Error creating GestureRecognizer:

--- src/app/(app)/learn/page.tsx
+++ src/app/(app)/learn/page.tsx
@@ -200,11 +200,11 @@ export default function LearnPage() {
     async function setup() {
       try {
         const vision = await FilesetResolver.forVisionTasks(
-          "/models/"
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.14/wasm"
         );
         const recognizer = await GestureRecognizer.createFromOptions(vision, {
           baseOptions: {
-            modelAssetPath: `/models/gesture_recognizer.task`,
+            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task`,
             delegate: "GPU",
           },
           runningMode: "VIDEO",
